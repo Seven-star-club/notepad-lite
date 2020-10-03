40dad6bef7530dcd2428ac563124e007534cc125
galanteria01 <shanuu12e@gmail.com>
2020-10-03 13:49:15 +0530

    Made a modification for purpose

--- app/src/main/java/com/shanu/notepadlite/databaseManager.kt
+++ app/src/main/java/com/shanu/notepadlite/databaseManager.kt
@@ -1,5 +1,10 @@
 package com.shanu.notepadlite
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
 class databaseManager {
     val dbName:String = "MyNotes"
     val dbTable = "Notes"
@@ -9,7 +14,29 @@ class databaseManager {
     val dbVersion = 1
     val sqlCreateTable = "CREATE TABLE IF NOT EXISTS" + dbTable + " (" + colID + " INTEGER PRIMARY KEY, "+
             colTitle + " TEXT, " + colDes + " TEXT);"
+    var sqlDB:SQLiteDatabase?=null
 
-    constructor(){
+    constructor(context: Context){
+        val db = DatabaseHelperNotes(context)
+        sqlDB = db.writableDatabase
+    }
+    inner class DatabaseHelperNotes: SQLiteOpenHelper {
+        var context:Context?=null
+        constructor(context:Context):super(context,dbName,null,dbVersion){
+            this.context = context
+        }
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL(sqlCreateTable)
+            Toast.makeText(this.context," Database is created ",Toast.LENGTH_SHORT).show()
+
+
+
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
